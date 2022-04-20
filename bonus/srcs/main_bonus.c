93353978bbb693c110a07cb16b3bd78e1de05b1c
lipemecprec <lipemecprec@yahoo.com.br>
2022-04-20 19:24:29 -0300

    bonus refactoring

--- bonus/srcs/main_bonus.c
+++ bonus/srcs/main_bonus.c
@@ -1,97 +1,175 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: faguilar <faguilar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/30 13:33:57 by faguilar          #+#    #+#             */
-/*   Updated: 2022/04/19 13:25:31 by faguilar         ###   ########.fr       */
+/*   Created: 2022/04/15 13:21:37 by faguilar          #+#    #+#             */
+/*   Updated: 2022/04/20 18:50:05 by faguilar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/pipex.h"
+#include "../include/pipex_bonus.h"
 
-void	new_cmd(t_cmd *cmd, char *cmd_str, char **path)
+void write_args(int argc, char **argv, char **envp)
+{
+	#include <string.h> 
+	int i = 0;
+	char arg_num[2];
+
+	arg_num[1] = 0;
+	write(1, "----\n", 6);
+	write(1, "argv > ", 6);
+	while (i < argc)
+	{
+		arg_num[0] = '0' + i;
+		write(1, "\t", 2);
+		write(1, arg_num, 2);
+		write(1, ">", 2);
+		write(1, argv[i], strlen(argv[i]));
+		if(!argv[i][0])
+			write(1, "VAZIO", 6);
+		i++;
+	}
+	write(1, "\n", 2);
+	write(1, "----\n", 6);
+}
+
+void	new_cmd(t_cmd *cmd, char *cmd_str, char **env_path)
 {
 	char	*try;
 	char	*temp;
 
-	cmd->cmd_array = ft_split(cmd_str, ' ');
-	cmd->path = NULL;
-	while (*path)
+	if(cmd_str)
 	{
-		temp = ft_strjoin(*path, "/");
-		try = ft_strjoin(temp, *cmd->cmd_array);
-		free(temp);
-		if (access(try, X_OK) == 0)
+		cmd->cmd_array = ft_split(cmd_str, ' ');
+		cmd->path = NULL;
+		while (*env_path)
 		{
-			cmd->path = ft_strdup(try);
+			temp = ft_strjoin(*env_path, "/");
+			try = ft_strjoin(temp, *cmd->cmd_array);
+			free(temp);
+			if (access(try, X_OK) == 0)
+			{
+				cmd->path = ft_strdup(try);
+				free(try);
+				break ;
+			}
 			free(try);
-			break ;
+			env_path++;
 		}
-		free(try);
-		path++;
 	}
 }
 
-void	set_cmds(char *argv[], char **path, t_cmd *cmd1, t_cmd *cmd2)
+char	*get_cmd_path(char *cmd_str, char **env_path)
 {
+	char	*try;
 	char	*temp;
+	char	**cmd_arr;
 
-	if (!argv[2])
-		terminate(139, NULL, NULL, NULL);
-	else
-		new_cmd(cmd1, argv[2], path);
-	if (!cmd1->path)
+	if(cmd_str)
 	{
-		temp = ft_strjoin(*cmd1->cmd_array, ERR_MSG_CMD);
-		write(STDERR, temp, ft_strlen(temp) + 1);
-		free(temp);
+		cmd_arr = ft_split(cmd_str, ' ');
+		while (*env_path)
+		{
+			temp = ft_strjoin(*env_path, "/");
+			try = ft_strjoin(temp, *cmd_arr);
+			free(temp);
+			if (access(try, X_OK) == 0)
+			{
+				free(cmd_arr);
+				return (try);
+			}
+			free(try);
+			env_path++;
+		}
 	}
-	new_cmd(cmd2, argv[3], path);
-	if (!cmd2->path)
+	return (NULL);
+}
+
+char	**get_cmd_args(char *cmd_str)
+{
+	char	**cmd_args;
+
+	if(ft_strlen(cmd_str)!= 0)
 	{
-		temp = ft_strjoin(*cmd2->cmd_array, ERR_MSG_CMD);
-		write(STDERR, temp, ft_strlen(temp) + 1);
-		free(temp);
+		cmd_args = ft_split(cmd_str, ' ');
+		return (cmd_args);
 	}
+	return (NULL);
+}
+
+void	set_cmds(char *argv[], t_pipex *data)
+{
+	char *temp;
+	
+	if (argv[2][0])
+		new_cmd(&data->cmd1, argv[2], data->env_path);
+	if (argv[3][0])
+		new_cmd(&data->cmd2, argv[3], data->env_path);
 }
 
 char	**get_envpath(char *envp[])
 {
-	char	**path;
+	char	**env_path;
 	char	*temp;
 
-	path = NULL;
+	env_path = NULL;
 	while (*envp)
 	{
 		if (ft_strnstr(*envp, "PATH=", 5))
 		{
 			temp = ft_strrchr(*envp, '=') + 1;
-			path = ft_split(temp, ':');
+			env_path = ft_split(temp, ':');
 			break ;
 		}		
 		*envp++;
 	}
-	return (path);
+	return (env_path);
 }
 
-void	set_files(char *argv[], t_cmd *cmd1, t_cmd *cmd2)
+void	set_files(char *argv[], t_pipex *data)
 {
-	cmd2->outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	data->outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (access(argv[1], F_OK) != 0)
-		terminate(OP_NOT_ALLOWED, NULL, cmd1, cmd2);
-	cmd1->infile = open(argv[1], O_RDONLY);
-	if (cmd1->infile == -1)
-		cmd1->infile = STDIN;
+		terminate(OP_NOT_ALLOWED, data);
+	dup2(data->outfile, STDOUT);
+	data->infile = open(argv[1], O_RDONLY);
+	if (data->infile == -1)
+		data->infile = STDIN;
+	dup2(data->infile, STDIN);
+}
+
+void	read_args(t_pipex *data, int argc, char *argv[], char *envp[])
+{
+	write_args(argc, argv, envp);
+	// if (argc != 5)
+	// 	terminate(WRONG_ARG_NO, data);
+	data->env_path = get_envpath(envp);
+	set_files(argv, data);
+	set_cmds(argv, data);
+}
+
+void	run_process(t_pipex *data, int *pipe, char *cmd_str, char *envp[])
+{
+	char	*cmd_path;
+	char	**cmd_args;
+
+	cmd_path = get_cmd_path(cmd_str, data->env_path);
+	cmd_args = get_cmd_args(cmd_str);
+	dup2(pipe[WRITE_END], STDOUT);
+	close(pipe[READ_END]);
+	close(pipe[WRITE_END]);
+	if (execve(cmd_path, cmd_args, envp) == -1)
+		terminate(errno, data);
 }
 
 // Desemperra:
 // Para o bônus, o processo pai direciona o STDIN para o pipe, e faz o fork
 // Dentro de um laço, enquanto houver argv, cada filho manda o STDOUT para o próximo processo
 // Depois de executar todos os filhos, o processo pai printa a saída.
-// 
+
 // 1 pipe para cada interprocesso.
 // Matar todos os filhos
 // estudar wait para pegar os erros dos processos.
@@ -101,28 +179,23 @@ int	main(int argc, char *argv[], char *envp[])
 	int		pid;
 	int		pid2;
 	t_pipex	data;
+	int		wstatus;
 
-	if (argc != 5 || !argv[2] || !argv[3] )
-		terminate(WRONG_ARG_NO, NULL, NULL, NULL);
-	data.path = get_envpath(envp);
-	set_files(argv, &data.cmd1, &data.cmd2);
-	set_cmds(argv, data.path, &data.cmd1, &data.cmd2);
-	dup2(data.cmd1.infile, STDIN);
-	// perror("MAIN > ");
-	dup2(data.cmd2.outfile, STDOUT);
+	read_args(&data, argc, argv, envp);
 	if (pipe(pipefd) == -1)
-		terminate(errno, data.path, &data.cmd1, &data.cmd2);
+		terminate(errno, &data);
+
 	pid = fork();
 	if (pid == -1)
-		terminate(errno, data.path, &data.cmd1, &data.cmd2);
+		terminate(errno, &data);
 	if (pid == 0)
 	{
-		dup2(pipefd[WRITE_END], STDOUT);
-		close(pipefd[READ_END]);
-		close(pipefd[WRITE_END]);
-		if (execve(data.cmd1.path, data.cmd1.cmd_array, envp) == -1)
-			terminate(errno, data.path, &data.cmd1, &data.cmd2);
-		// printf("pid = 0 > err code: %d | err msg %s", errno, strerror(errno));
+		run_process(&data, pipefd, argv[2], envp);
+		// dup2(pipefd[WRITE_END], STDOUT);
+		// close(pipefd[READ_END]);
+		// close(pipefd[WRITE_END]);
+		// if (execve(data.cmd1.path, get_cmd_args(argv[2]), envp) == -1)
+		// 	terminate(errno, &data);
 	}
 	pid2 = fork();
 	if (pid2 == -1)
@@ -132,15 +205,14 @@ int	main(int argc, char *argv[], char *envp[])
 		dup2(pipefd[READ_END], STDIN);
 		close(pipefd[READ_END]);
 		close(pipefd[WRITE_END]);
-		if (execve(data.cmd2.path, data.cmd2.cmd_array, envp) == -1)
-			terminate(errno, data.path, &data.cmd1, &data.cmd2);
-		// printf("pid2 = 0 > err code: %d | err msg %s", errno, strerror(errno));
+		if (execve(data.cmd2.path, get_cmd_args(argv[3]), envp) == -1)
+			terminate(errno, &data);
 	}
 	close(pipefd[READ_END]);
 	close(pipefd[WRITE_END]);
 	waitpid(pid, NULL, 0);
 	if (!data.cmd2.path)
-		terminate(ERR_NO_JOB, data.path, &data.cmd1, &data.cmd2);
-	terminate(EXIT_SUCCESS, data.path, &data.cmd1, &data.cmd2);
+		terminate(ERR_NO_JOB, &data);
+	terminate(EXIT_SUCCESS, &data);
 	return (0);
 }
